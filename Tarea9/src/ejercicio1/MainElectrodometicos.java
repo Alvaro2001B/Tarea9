082ee1a269523749a34c394115f3605a0cfc8299
Alvaro2001B <100117640+Alvaro2001B@users.noreply.github.com>
2022-09-13 09:39:30 +0200

    Arreglos

---
+++ Tarea9/src/ejercicio1/MainElectrodometicos.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+import java.util.ArrayList;
+
+public class MainElectrodometicos {
+
+	public static void main(String[] args) {
+		ArrayList<Electrodomestico> listElectro=new  ArrayList<Electrodomestico>();
+		Lavadora p=new Lavadora();
+		listElectro.add(p);
+	}
+
+}
